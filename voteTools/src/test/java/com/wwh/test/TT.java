2c5c61b35903814288cc0ee034fe5913b4752f91
ww <wangwen135@gmail.com>
2015-09-25 17:42:52 +0800

    测试

--- voteTools/src/test/java/com/wwh/test/TT.java
+++ voteTools/src/test/java/com/wwh/test/TT.java
@@ -5,18 +5,20 @@ import java.util.Date;
 import com.alibaba.fastjson.JSON;
 
 public class TT {
-	
-	public static void main(String[] args) {
-		String ss = "{\"state\":false,\"error\":\"\u9a8c\u8bc1\u7801\u4e0d\u6b63\u786e\"}";
-		
-		Object o = JSON.parse(ss);
-		System.out.println(o);
-	}
-	public static void main1(String[] args) {
-		Date d = new Date(1443025656052l);
-		
-		Date d1 = new Date(1443025844974l);
-		System.out.println(d.toLocaleString());
-		System.out.println(d1.toLocaleString());
-	}
+
+    public static void main(String[] args) {
+        String ss = "{\"state\":false,\"error\":\"\u9a8c\u8bc1\u7801\u4e0d\u6b63\u786e\"}";
+
+        Object o = JSON.parse(ss);
+        System.out.println(o);
+    }
+
+    @SuppressWarnings("deprecation")
+    public static void main1(String[] args) {
+        Date d = new Date(1443025656052l);
+
+        Date d1 = new Date(1443025844974l);
+        System.out.println(d.toLocaleString());
+        System.out.println(d1.toLocaleString());
+    }
 }
